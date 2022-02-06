d36ed26b9f08741ff38c45e95ac0a629ab4046e5
Philip Andreas Kingo <philipandreaskingo@hotmail.com>
2022-02-06 20:31:29 +0100

    added kirkeblade

--- app/routes/kirkeblad.tsx
+++ app/routes/kirkeblad.tsx
@@ -1,3 +1,42 @@
-const Kirkeblad = () => <div>Kirkeblad</div>;
+import { LoaderFunction, useLoaderData } from "remix";
+import { PageHeader } from "~/components/PageHeader";
+import { getChurchPaper } from "~/utils/getChurchPapers";
+
+export const loader: LoaderFunction = async () => {
+  const papers = await getChurchPaper();
+  return papers;
+};
+
+type PaperItem = {
+  title: string;
+  file: string;
+};
+
+const PaperItem: React.FC<PaperItem> = ({ file, title }) => (
+  <li className="list-disc">
+    <a
+      href={file}
+      className="font-bold text-green-main hover:underline"
+      target="_blank"
+    >
+      {title}
+    </a>
+  </li>
+);
+
+const Kirkeblad = () => {
+  const data = useLoaderData<PaperItem[]>();
+  return (
+    <div>
+      <PageHeader>Kirkeblad</PageHeader>
+      <p>Her finder du altid sidste nye kirkeblad.</p>
+      <ul className="pl-4 mt-2">
+        {data.map((item) => (
+          <PaperItem key={item.file} {...item} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Kirkeblad;
---
+++ app/utils/getChurchPapers.ts
@@ -0,0 +1,28 @@
+import fs from "fs/promises";
+import path from "path";
+import parseFrontMatter from "front-matter";
+
+export async function getChurchPaper() {
+  const paperPath = await fs.readdir(`${__dirname}/../content/church-paper`, {
+    withFileTypes: true,
+  });
+
+  const papers = await Promise.all(
+    paperPath.map(async (dirent) => {
+      const file = await fs.readFile(
+        path.join(`${__dirname}/../content/church-paper`, dirent.name)
+      );
+      const { attributes } = parseFrontMatter<{
+        title: string;
+        file: string;
+      }>(file.toString());
+      return {
+        slug: dirent.name.replace(/\.md/, ""),
+        title: attributes.title,
+        file: attributes.file,
+      };
+    })
+  );
+
+  return papers;
+}
